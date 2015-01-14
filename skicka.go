e22b7520bdf11a3aecf811af10fb45cac233eaf9
Matt Pharr <mpharr@google.com>
2015-01-13 21:16:31 -0800

    Make error message match others

--- skicka.go
+++ skicka.go
@@ -2143,7 +2143,7 @@ func compileUploadFileTree(localPath, driveRoot string, encrypt bool) ([]LocalTo
 		}
 
 		if (info.Mode() & os.ModeSymlink) != 0 {
-			log.Printf("Ignoring symlink \"%s\".", path)
+			fmg.Printf("skicka: ignoring symlink \"%s\".", path)
 			return nil
 		}
 
